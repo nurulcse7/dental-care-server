c461345d7eaaf3f00c20ebce2bb6f4b0b13e882a
nurulcse7 <nurul.cse7@gmail.com>
2022-12-14 19:48:51 +0600

    User create and save in database

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-
 const app = express();
 
 // middleware
@@ -13,10 +13,27 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.70yiu6o.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1,});
 
+// JWT middleware
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      return res.status(401).send('unauthorized access');
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+      if (err) {
+          return res.status(403).send({ message: 'forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+  })
+}
+
 async function run() {
   try {
     const appointmentOptionCollection = client.db('dentalCare').collection('appointmentOptions');
     const bookingsCollection = client.db('dentalCare').collection('bookings');
+    const usersCollection = client.db('dentalCare').collection('users');
 
     // get the all appointmentOptions from mongodb(appointmentOptions) 74-2
     app.get('/appointmentOptions', async (req, res) => {
@@ -109,6 +126,72 @@ async function run() {
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     });
+
+    // 75-2 get email address from bookings  verifyJWT,
+    app.get('/bookings',  async (req, res) => {
+      const email = req.query.email;
+      // const decodedEmail = req.decoded.email;
+      // if (email !== decodedEmail) {
+      //     return res.status(403).send({ message: 'forbidden access' });
+      // }
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+  })
+
+  app.get('/jwt', async (req, res) => {
+    const email = req.query.email;
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '7d' })
+        return res.send({ accessToken: token });
+    }
+    res.status(403).send({ accessToken: '' })
+});
+
+// store user (save user in database when user signup) 75-3
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  console.log(user);
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
+
+// user show to ui 
+app.get('/users', async (req, res) => {
+    const query = {};
+    const users = await usersCollection.find(query).toArray();
+    res.send(users);
+});
+// // if user not an admin, he can't access dashboard
+app.get('/users/admin/:email', async (req, res) => {
+    const email = req.params.email;
+    const query = { email }
+    const user = await usersCollection.findOne(query);
+    res.send({ isAdmin: user?.role === 'admin' });
+})
+
+// user update 
+app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+    const decodedEmail = req.decoded.email;
+    const query = { email: decodedEmail };
+    const user = await usersCollection.findOne(query);
+    if (user?.role !== 'admin') {
+        return res.status(403).send({ message: 'forbidden access' })
+    }
+    const id = req.params.id;
+    const filter = { _id: ObjectId(id) }
+    const options = { upsert: true };
+    const updatedDoc = {
+        $set: {
+            role: 'admin'
+        }
+    }
+    const result = await usersCollection.updateOne(filter, updatedDoc, options);
+    res.send(result);
+})
+
   } finally {
   }
 }
